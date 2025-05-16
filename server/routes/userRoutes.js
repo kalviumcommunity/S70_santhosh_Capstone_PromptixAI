f965e89350e699426cffccd4d8e24725e04b73b1
santhoshjr007 <santhosh.r@kalvium.community>
2025-05-16 14:10:43 +0530

    postrq

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ import { registerUser, loginUser, userCredits } from '../controllers/userControl
 import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router();
+userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.post('/credits', userAuth,userCredits);
 export default userRouter;
